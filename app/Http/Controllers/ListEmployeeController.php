5661a995256c6359a9be149724588ea5f6c47919
松永裕太郎 <yutaro_matsunaga@yaz.co.jp>
2022-08-05 05:52:00 +0900

    ControllerとViewを修正

--- app/Http/Controllers/ListEmployeeController.php
+++ app/Http/Controllers/ListEmployeeController.php
@@ -2,11 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Employee;
 use Illuminate\Http\Request;
 
 class ListEmployeeController extends Controller
 {
 	public function index() {
-		return view('list_employee');
+
+		$employee_all = Employee::all();
+
+		// DBから取得してきたデータ（$employee_all）をViewへ（employeeとして）受け渡す
+		return view('list_employee', ['employees' => $employee_all]);
 	}
 }
--- resources/views/list_employee.blade.php
+++ resources/views/list_employee.blade.php
@@ -3,11 +3,22 @@
     <title>Menu</title>
   </head>
   <body>
-	<h1>社員一覧画面</h1>
-	<h2>一覧です</h2>
-	<ul>
-		<li>社員ID</li>
-		<li>社員名</li>
-	</ul>
-  </body>
+		<h1>社員一覧画面</h1>
+		<table border="10">
+			<tr><!-- 行1（見出し）-->
+				<th>社員ID</th> <th>社員名</th> <th>所属セクション</th> <th>メールアドレス</th> <th>性別</th>
+			</tr>
+
+			@foreach ($employees as $item)
+			<tr><!-- データ（繰り返し） -->
+				<td>{{ $item -> employee_id }}</td>
+				<td>{{ $item -> getFullName() }}</td>
+				<td>{{ $item -> getSection() }}</td>
+				<td>{{ $item -> mail }}</td>
+				<td>{{ $item -> getGender() }}</td>
+			</tr>
+			@endforeach
+
+		</table>
+	</body>
 </html>
