bc55b7cedce340ba6d3d8df7c75325d920c18d4c
松永裕太郎 <yutaro_matsunaga@yaz.co.jp>
2022-08-04 10:50:20 +0900

    コントローラの追加 ※[php artisan make:controller]で作成しました。

---
+++ app/Http/Controllers/RegistEmployeeController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RegistEmployeeController extends Controller
+{
+	public function index() {
+		return view('regist_employee');
+	}
+}
